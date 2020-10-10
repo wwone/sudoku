f8782252f396be73f92abb9ed2c96cd3c3cae975
Bob Swanson <40324245+wwone@users.noreply.github.com>
2020-10-10 15:27:46 -0500

    Add compilation script
    The compilation script will note that required location
    of the FOP JAR files, as well as the BFOJson JAR
    file.

---
+++ src/compit.sh
@@ -0,0 +1,62 @@
+#
+# compile the Sudoku code
+#
+# several dependencies:
+# 1) FOP must be installed in both the
+#    compilation and execution directory
+#    see below for references to the "build"
+#    and "lib" directories, full of many
+#    JAR files
+#
+# 2) bfojson-1.jar must be present for
+#    both compilation and execution
+#    This project is on GitHub as BFOJson
+#    see...  https://github.com/faceless2/json
+#
+# 3) Some sudoku code does math operations. We
+#    may or may not really want them, but
+#    they are present. Sooo,
+#        commons-math3-3.4.1.jar
+#    is needed
+#
+# The "lint" setting is present to be in good
+# coordination with the BFOJson library
+#
+#
+JAV=javac
+
+#LINT=
+##LINT=" -Xlint:deprecation -Xlint:unchecked "
+LINT=" -Xlint:deprecation  "
+
+
+# following for FOP code
+MAIN=build/fop.jar
+
+# following for FOP code
+CP=$CP:$MAIN
+CP=$CP:lib/avalon-framework-api-4.3.1.jar
+CP=$CP:lib/avalon-framework-impl-4.3.1.jar
+CP=$CP:lib/batik-all-1.9.jar
+CP=$CP:lib/commons-io-1.3.1.jar
+CP=$CP:lib/commons-logging-1.0.4.jar
+CP=$CP:lib/fontbox-2.0.4.jar
+CP=$CP:lib/serializer-2.7.2.jar
+CP=$CP:lib/xalan-2.7.2.jar
+CP=$CP:lib/xercesImpl-2.9.1.jar
+CP=$CP:lib/xml-apis-1.3.04.jar
+CP=$CP:lib/xml-apis-ext-1.3.04.jar
+CP=$CP:lib/xmlgraphics-commons-2.2.jar
+
+# following for JSON
+CP=$CP:bfojson-1.jar
+
+# following for sudoku code
+CP=$CP:commons-math3-3.4.1.jar
+CP=$CP:.
+
+##OBJ=test
+OBJ=Sud
+
+echo $JAV $LINT -classpath $CP $OBJ.java
+$JAV $LINT -classpath $CP $OBJ.java
